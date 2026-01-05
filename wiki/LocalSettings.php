314e43d1f72f101820bcb303bf2a8d923adf66ac
Ian <ian@example.com>
2026-01-05 01:41:25 +0000

    Enable more wiki skins

--- wiki/LocalSettings.php
+++ wiki/LocalSettings.php
@@ -194,9 +194,9 @@ wfLoadExtension( 'WikiEditor' );
 
 
 # -- Styles --
-// wfLoadSkin( 'MinervaNeue' );
 // wfLoadSkin( 'MonoBook' );
-// wfLoadSkin( 'Timeless' );
+wfLoadSkin( 'MinervaNeue' );
+wfLoadSkin( 'Timeless' );
 wfLoadSkin( 'Vector' );
 $wgDefaultSkin = "Vector";
 
